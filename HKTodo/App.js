19952b1c304c77754a878ffa4a741410c3d5418b
anomalicoder <hwiunkim@gmail.com>
2023-07-21 01:35:27 +0900

    touchable, pressable
    Touchables
    -TouchableOpacity
    -TouchableHighlight
    -TouchableWithoutFeedback
    
    Pressable(More options, props to operate)

--- HKTodo/App.js
+++ HKTodo/App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity, 
+  TouchableHighlight, 
+  TouchableWithoutFeedback 
+} from 'react-native';
+import {theme} from './colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableHighlight 
+          underlayColor="teal"
+          onPress={() => console.log("pressed")}>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
@@ -13,8 +30,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal:40,
   },
+  header:{
+    justifyContent:'space-between',
+    flexDirection:"row",
+    marginTop:100,
+
+  },
+  btnText:{
+    color:"white",
+    fontSize:40,
+    fontWeight:"500",
+  }
 });
